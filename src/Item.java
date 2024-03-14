69fcb8a6741008630217aa89bc47930a0811fa92
dviet04 <121754515+dviet04@users.noreply.github.com>
2024-03-14 07:52:39 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,30 @@
-public class Item {
+public abstract class Item {
+    public String nameItem;
+    private String descriptionItem;
+
+    protected String ID_Item;
+
+    float priceItem;
+
+    public String getNameItem() {
+        return nameItem;
+    }
+
+    public void setNameItem(String nameItem) {
+        this.nameItem = nameItem;
+    }
+
+    public String getDescriptionItem() {
+        return descriptionItem;
+    }
+
+    public void setDescriptionItem(String descriptionItem) {
+        this.descriptionItem = descriptionItem;
+    }
+    public void Item (String nameItem, String descriptionItem, String ID_Item, float priceItem) {
+        this.nameItem = nameItem;
+        this.descriptionItem = descriptionItem;
+        this.ID_Item = ID_Item;
+        this.priceItem = priceItem;
+    }
 }
