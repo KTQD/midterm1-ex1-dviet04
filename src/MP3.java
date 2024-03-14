0cff5b35462f885a6f0441ea64bf4de9ce8571e9
dviet04 <121754515+dviet04@users.noreply.github.com>
2024-03-14 08:02:13 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,17 @@
-public class MP3 {
+public class MP3 extends Item{
+    public double duration;
+
+
+    public MP3(String nameItem, String descriptionItem, String ID_Item, float priceItem, double duration) {
+        super(nameItem, descriptionItem, ID_Item, priceItem);
+        this.duration = duration;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
 }
