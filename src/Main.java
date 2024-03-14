740c05ffb1393286f7b7876d4ce41e013ebc50fb
dviet04 <121754515+dviet04@users.noreply.github.com>
2024-03-14 08:20:24 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
-        mp3.showInfo();
-
-        Book book;
-        book.showInfo();
-
+        MP3 mp3 = new MP3("CD", "plastic", "001", 10, 5.5);
+        Book book1 = new Book("Life", "Good", "book1", 200, "John", 350, "21");
+        mp3.showInFo();
+        book1.showInFo();
     }
 }
